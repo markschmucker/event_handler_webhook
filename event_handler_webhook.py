901da21211e1015acbd3a41d063f3318927a932a
markschmucker <36678240+markschmucker@users.noreply.github.com>
2021-04-03 10:36:54 +0930

    add post_event handler, just emails for now

--- event_handler_webhook.py
+++ event_handler_webhook.py
@@ -36,9 +36,11 @@ def dict_factory(cursor, row):
 @app.route('/topic_event', methods=['POST'])
 def topic_event_handler():
 
-    # Currently we're only interested in topic_created. Other webhooks are available,
-    # for users, topics, and posts. (However not for user_added_to_group). See
-    # https://meta.discourse.org/t/setting-up-webhooks/49045. See webhook page.
+    # See https://meta.discourse.org/t/setting-up-webhooks/49045
+    # See also https://forum.506investorgroup.com/admin/api/web_hooks
+
+    # This handler handles new topics and flags the new topic for moderator review.
+    # Mods will verify it has the right tags, is in the right category, etc.
 
     event_type = request.headers['X-Discourse-Event-Type']
     event = request.headers['X-Discourse-Event']
@@ -77,6 +79,61 @@ def topic_event_handler():
 
             # Note the flag method is currently added to client.py, not a subclass client506.py.
             client.flag(post_id, msg)
+
+        return '', 200
+    else:
+        return '', 400
+
+
+@app.route('/post_event', methods=['POST'])
+def post_event_handler():
+
+    # This handler handles new posts. If the post appears to contain wiring instructions,
+    # flag the new topic for moderator review. Mods will add a staff notice warning about
+    # wiring scams, if appropriate. This check does not need to be perfect- it can never
+    # be perfect and it's easy enough for mods to ignore any false alarms.
+
+    event_type = request.headers['X-Discourse-Event-Type']
+    event = request.headers['X-Discourse-Event']
+    print 'event: ', event
+
+    # Any checks for category are best done in the webhook settings
+    if event_type == 'post' and event == 'post_created':
+
+        post = request.json['post']
+
+        # Checks to make sure it's a normal public topic, not a PM or system message
+        created_by = post['username']
+        post_type = post['post_type']  # 1 is "regular". Does that exclude PMs? Need to test.
+
+        if post_type == 1:
+            cooked = post['cooked']
+
+
+
+            # tags = topic.get('tags', [])
+            # topic_id = topic['id']
+            # title = topic['title']
+            # slug = topic['slug']
+            # # Could edit the webhook to deliver only Deals category
+            # # category_id = topic['category_id']
+            # url = "https://forum.506investorgroup.com/t/%s/%d" % (slug, topic_id)
+            #
+            # msg = '**[How to Review a New Topic](https://forum.506investorgroup.com/t/moderators-reviewing-each-new-topic/18317)**  ' \
+            #       '@%s created a new topic: \"%s\".  ' \
+            #       'Review here: %s.  ' % \
+            #       (created_by, title, url)
+
+            msg = "Ïnterpret this"
+
+            send_simple_email('markschmucker@yahoo.com', event, msg)
+
+            # client = create_client(1)
+            # post = client.post(topic_id, 1)
+            # post_id = post['post_stream']['posts'][0]['id']
+            #
+            # # Note the flag method is currently added to client.py, not a subclass client506.py.
+            # client.flag(post_id, msg)
 
         return '', 200
     else:
